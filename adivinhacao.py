2c9f847cc449d9f11b77cd5c6119d0e790d21702
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-10-22 17:55:48 -0300

    refactor: melhorias de lebibilidade e acréscimo do elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,14 +4,23 @@ print("##################################")
 
 numero_secreto = 42
 
-chute = input("digite o seu número: ")
+chute_string = input("digite o seu número: ")
 
-print("Você digitou ", chute)
+print("Você digitou ", chute_string)
 
-if numero_secreto == int(chute):
+chute = int(chute_string)
+
+acertou = chute == numero_secreto
+chute_maior = chute > numero_secreto
+chute_menor = chute < numero_secreto
+
+if acertou:
     print("Você acertou!")
 else:
-    print("Você errou! O número correto é ", numero_secreto)
+    if chute_maior:
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif chute_menor:
+        print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("##################################")
 print("Fim de jogo... Até a próxima!")
