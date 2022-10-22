b56f38a6c42d08c27fb3d8592ecfe6c697f7d2e7
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-10-22 20:44:03 -0300

    refactor: utilizando for, break, contine

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,9 +6,10 @@ numero_secreto = 42
 total_tentativas = 3
 rodada = 1
 
-while rodada <= total_tentativas:
-    print("Tentativa: ", rodada, " de ", total_tentativas)
+for rodada in range(1, total_tentativas + 1):
+    print("Tentativa: {} de {}".format(rodada, total_tentativas))
     chute_string = input("digite o seu número: ")
+    print("Você deve digitar um número entre 1 e 100")
     print("Você digitou ", chute_string)
     chute = int(chute_string)
 
@@ -16,15 +17,20 @@ while rodada <= total_tentativas:
     chute_maior = chute > numero_secreto
     chute_menor = chute < numero_secreto
 
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+
     if acertou:
+        rodada = 4
         print("Você acertou!")
+        break
     else:
         if chute_maior:
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif chute_menor:
             print("Você errou! O seu chute foi menor do que o número secreto.")
-        rodada = rodada + 1
 
-    print("##################################")
-    print("Fim de jogo... Até a próxima!")
-    print("##################################")
+print("##################################")
+print("Fim de jogo... Até a próxima!")
+print("##################################")
