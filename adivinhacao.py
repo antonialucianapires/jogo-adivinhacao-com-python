2ddb466b8b64544aaf2f2d854b2e3a9e6f2d2941
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-10-22 18:15:04 -0300

    refactor: utilizando while para limitar tentativas

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,25 +3,28 @@ print("Bem vinde ao jogo de Adivinhação! 🚀")
 print("##################################")
 
 numero_secreto = 42
+total_tentativas = 3
+rodada = 1
 
-chute_string = input("digite o seu número: ")
+while rodada <= total_tentativas:
+    print("Tentativa: ", rodada, " de ", total_tentativas)
+    chute_string = input("digite o seu número: ")
+    print("Você digitou ", chute_string)
+    chute = int(chute_string)
 
-print("Você digitou ", chute_string)
+    acertou = chute == numero_secreto
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
 
-chute = int(chute_string)
+    if acertou:
+        print("Você acertou!")
+    else:
+        if chute_maior:
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif chute_menor:
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+        rodada = rodada + 1
 
-acertou = chute == numero_secreto
-chute_maior = chute > numero_secreto
-chute_menor = chute < numero_secreto
-
-if acertou:
-    print("Você acertou!")
-else:
-    if chute_maior:
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif chute_menor:
-        print("Você errou! O seu chute foi menor do que o número secreto.")
-
-print("##################################")
-print("Fim de jogo... Até a próxima!")
-print("##################################")
+    print("##################################")
+    print("Fim de jogo... Até a próxima!")
+    print("##################################")
